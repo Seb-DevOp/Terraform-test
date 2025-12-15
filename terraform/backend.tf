666755c7b03dd4bb5da757b40332be70b0e0a68e
Seb Portable <sebastien.giguet@ynov.com>
2025-12-15 15:55:32 +0100

    first_ciCd

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -4,9 +4,10 @@ terraform {
     prefix = "stockage_de_TFstate"
     key    = "Rocky_VM.tfstate"
   }
-}
+
   
   provider "google" {
   project = var.gcp_project_id
   region  = var.gcp_region
+}
 }
