cb85657b64fd7f289fbd3b839aec604f3e7bf956
Seb Portable <sebastien.giguet@ynov.com>
2025-12-16 10:43:34 +0100

    first_ciCd

--- modules/main.tf
+++ modules/main.tf
@@ -1,1 +1,21 @@
-##
+resource "google_compute_instance_template" "default" {
+  name        = "TestTerraform-variableICI"
+  description = "This template is used to create VM."
+
+  tags = ["Lenny", "est", "le", "plus", "bo"]
+
+  instance_description = "description assigned to instances"
+  machine_type         = "e2-medium"
+  can_ip_forward       = false
+
+  // Create a new boot disk from an image
+  disk {
+    source_image      = "debian-cloud/debian-11"
+    auto_delete       = true
+    boot              = true
+
+  network_interface {
+    network = "default"
+  }
+  }
+}
---
+++ terraform/prod/Prod.tfvars
@@ -0,0 +1,3 @@
+gcp_project_id = "terraform-test-481313"
+gcp_region     = "europe-west1"
+gcp_zone       = "europe-west1-b"
